b25c422303f5e7bab2c4c424a1d6f7300e4f34a6
John Maybee <john.d.maybee@gmail.com>
2022-11-22 22:03:58 -0500

    end of class

--- server/server.js
+++ server/server.js
@@ -5,6 +5,28 @@ const port = 5001;
 
 app.use(express.static('server/public'));
 
+const quoteList = require('./modules/quoteList');
+
 app.listen(port, () => {
     console.log('listening on port, ', port);
-})
+});
+
+app.get('/quotes', function(req, res) {
+    console.log("request for /quotes was made");
+    res.send(quoteList);
+    // res.sendStatus(418);
+} );
+
+
+// control + c closes the server
+// route, path, URL (can be used rather interchangably)
+// .get is an example of the verb. what we are doing. the method.
+
+///////////////////////////////////////////////////CRUD////////////////////////////////////////////////////
+
+// this is what makes a CRUD application.
+// create(post) read(get) update(put) delete(delete)
+// C - create - post
+// R - read - get
+// U - update - put
+// D - delete - delete
